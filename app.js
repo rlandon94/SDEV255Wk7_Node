94f071154bd59e79163b2edd4d0b6c2c1f4ec866
Your Name <you@example.com>
2023-10-09 17:27:51 -0400

    week7

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
 const { result } = require('lodash');
+const blogRoutes = require('./routes/blogRoutes');
 
 //express app
 const app = express();
@@ -28,71 +28,16 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
     res.redirect('/blogs');
     
-    //res.send('<p>home page</p>');
-    //res.sendFile('./view/index.html', { root: __dirname})
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About'});
-    //res.send('<p>about page</p>');
-    //res.sendFile('./view/about.html', { root: __dirname})
 });
 
 //blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-        res.render('index', { title: 'All Blogs', blogs: result })
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
+app.use('/blogs', blogRoutes);
 
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
 
-    blog.save()
-    .then(result => {
-        res.redirect('/blogs');
-    })
-    .catch(err => {
-        console.log(err);
-    });
-})
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: 'Blog Details' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-  
-
-app.get('/blogs/create', (req,res) => {
-    res.render('create', { title: 'Create a new blog'});
-})
-
-// //redirect
-// app.get('/about-us', (req,res) => {
-//     res.redirect('/about');
-// })
 
 //404 page
 app.use((req, res) => {
